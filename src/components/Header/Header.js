e24feba9d25cb9af1c2d16af9f9bbf79784d5e18
Dillon <dillonrhamilton@gmail.com>
2015-11-25 23:01:57 -0500

    figure out starter?

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,12 +15,12 @@ class Header extends Component {
         <div className="Header-container">
           <a className="Header-brand" href="/" onClick={Link.handleClick}>
             <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
+            <span className="Header-brandTxt">Rune App Testing</span>
           </a>
           <Navigation className="Header-nav" />
           <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
+            <h1 className="Header-bannerTitle">Rune App</h1>
+            <p className="Header-bannerDesc">See what runes you use!</p>
           </div>
         </div>
       </div>
