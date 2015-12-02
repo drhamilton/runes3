a1d1b15f8e03db2b66ce8ea466269e126105b261
Dillon <dillonrhamilton@gmail.com>
2015-12-01 22:51:47 -0500

    fakshdfkjas

--- src/components/UsernameForm/UsernameForm.js
+++ src/components/UsernameForm/UsernameForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 
 class UsernameForm extends Component {
     state = { name: '' };
@@ -7,9 +8,26 @@ class UsernameForm extends Component {
         this.setState({name: name});
         console.log('state is', this.state);
     }
+    getSummonerId = (name) => {
+        let api = 'df0f2c4b-4e06-40c4-86c8-1731f1dd2d60';
+        let url = `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${name}?api_key=${api}`;
+
+        request
+            .get('/test')
+            .set('Accept', 'application/json')
+            .end(function(err, res){
+                console.log('error', err);
+                console.log('res', res);
+            });
+    }
+
     handleSubmit = (ev) => {
         ev.preventDefault();
-        this.setName(ev.target[0].value);
+
+        const name = ev.target[0].value;
+
+        this.setName(name);
+        this.getSummonerId(name);
     }
 
     render() {
